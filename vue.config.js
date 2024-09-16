de5d8a2f44201304182ac4f71cce68f0b8c902bf
Stefano Scarpa <scarpastefano@icloud.com>
2024-09-16 16:20:42 +0200

    build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-    publicPath: './'
-  }
+    publicPath: './',  // Questo risolve il problema dei percorsi relativi
+    configureWebpack: {
+      resolve: {
+        extensions: ['.js', '.vue', '.json'],  // Assicura che Webpack gestisca correttamente i file Vue e JS
+      },
+    },
+  };
   
